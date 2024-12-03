a16318ddbad2d730273ebcfd8a6f1fe9f602244c
Thomas Aglassinger <roskakori@users.sourceforge.net>
2024-12-03 20:39:26 +0100

    #6 Clean up naming

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,3 @@
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from main import app
 from tasks import Task
@@ -12,10 +11,12 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
+
 def test_read_tasks():
     response = client.get("/tasks")
     assert response.status_code == 200
     assert len(response.json()) == 5
+
 
 def test_get_task_by_id():
     response = client.get("/tasks/1")
@@ -26,10 +27,10 @@ def test_get_task_by_id():
 
 def test_add_and_expect_one_more_than_before():
     def count():
-        response = client.get("/tasks")
-        return len(response.json())
+        task_list_response = client.get("/tasks")
+        return len(task_list_response.json())
 
-    c1 = count()
-    res = client.post("/tasks", json={"title": "Buy milk", "category": "shopping"})
-    assert count() == c1 + 1
-    Task(**res.json())
+    initial_task_count = count()
+    response = client.post("/tasks", json={"title": "Buy milk", "category": "shopping"})
+    assert count() == initial_task_count + 1
+    Task(**response.json())
